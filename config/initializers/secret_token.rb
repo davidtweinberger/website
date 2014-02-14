68e54d3e6489252dc6c04293a9ed3159e3c904ac
David Weinberger <davidtweinberger@Davids-Mac.local>
2014-02-14 11:34:18 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Davidweinberger::Application.config.secret_key_base = '5a9aa3006642395ae748426eda3b737f78a0c6a155a5ee0af13cd92f38eb99667f47efcaad5b58dfb089b87f6f84471d46f5376bb39ac67dd3b0fd027ddadbd5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Davidweinberger::Application.config.session_store :cookie_store, key: '_davidweinberger_session'
